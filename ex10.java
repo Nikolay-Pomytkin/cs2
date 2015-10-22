467a7cf56fec872888fe52a35e4c47f6f32ac142
Nikolay Pomytkin <nikolayp2800@gmail.com>
2015-10-22 09:43:53 -0400

    eat a frog cuz teo is gay

---
+++ ex10.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ex10 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+	}
+	public static String checkPoint (int x1, int y1, int x2, int y2, int x3, int y3){
+		String f = "frog";
+		return f;
+	}
+}
