6f6c71828cca2afaa70214a984231db37c2faea7
Nikolay Pomytkin <nikolayp2800@gmail.com>
2015-11-12 09:15:10 -0500

    Create apartment.java

---
+++ ex/apartment.java
@@ -0,0 +1,9 @@
+public class apartment{
+  private int rent;
+  private int numRooms;
+  private int numOccupants;
+  private boolean centralAir;
+  public void setRent(){
+  }
+Rent, num rooms, num occupants, central air
+}
