4d2cd578a749485e865d13821738f3006ccc73c3
Nikolay Pomytkin <nikolayp2800@gmail.com>
2015-11-25 09:18:53 -0500

    Create twoA.java

---
+++ ex/twoA.java
@@ -0,0 +1,13 @@
+package twoa;
+import java.util.Scanner;
+public class TwoA {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the length of the first leg of your right triangle:");
+        int a = scan.nextInt();
+        System.out.println("Enter the length of the second leg of your right triangle:");
+        int b = scan.nextInt();
+        double hypotenuse = Math.sqrt((a*a)+(b*b));
+        System.out.println("The hyponetuse: " + hypotenuse);
+    }    
+}
