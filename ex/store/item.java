614f95d98de7149dc027e84db0ffec2f0218c452
Nikolay Pomytkin <nikolayp2800@gmail.com>
2016-01-07 08:56:10 -0500

    Update item.java

--- ex/store/item.java
+++ ex/store/item.java
@@ -1,1 +1,11 @@
-
+public class Item {
+  private String description;
+  private double buyer_price;
+  private double sales_price;
+  
+  public Item(String description, double buyer_price, double sales_price) {
+    description = description;
+    buyer_price = buyer_price;
+    sales_price = sales_price;
+  }
+}
