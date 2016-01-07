0ddb2734604e9bf578ac0282ad0fe05e55721250
Nikolay Pomytkin <nikolayp2800@gmail.com>
2016-01-07 08:58:52 -0500

    Update run.java

--- ex/store/run.java
+++ ex/store/run.java
@@ -1,1 +1,8 @@
+public class run {
 
+    public static void main(String[] args) {
+      Item[] items = new Item[3];
+      Store s = new Store("NYC", items, "Nik", "123-456-7890");
+      System.out.println{"Location: " + Store.getLocation() + "; Name: " + Store.getName() + "; Phone number: " + Store.getPhoneNumber()}
+    }
+}
