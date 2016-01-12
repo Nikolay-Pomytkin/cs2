0fe59da608a2e9e51a7468249354078bbcdf7aeb
Nikolay Pomytkin <nikolayp2800@gmail.com>
2016-01-12 10:01:22 -0500

    Update storeproject.java

--- ex/groupStore/storeproject.java
+++ ex/groupStore/storeproject.java
@@ -1,9 +1,12 @@
-import java.util.scanner;
-public class storeproject{
+package store;
+import java.util.Scanner;
+import store.store;
+import store.item;
+public class storeProject{
   private static void main(String [] args){
     Boolean running = true;
     Scanner scan = new Scanner(System.in);
-    Store s = new Store();
+    store s = new store();
     System.out.println("What is the name of your store?");
     s.setName(scan.next());
     System.out.println("Where is your store located?");
@@ -11,7 +14,7 @@ public class storeproject{
     System.out.println("Who is the owner of your store?");
     s.setOwner(scan.next());
     System.out.println("What the the phone number of your store?");
-    s.setPhonenumber(scan.nextInt());
+    s.setPhoneNumber(scan.next());
     while(running == true){
       System.out.println("Select an option (1-4):");
       System.out.println("1: Add an item to your store");
@@ -21,7 +24,10 @@ public class storeproject{
       System.out.println("5: Exit this application");
       int option = scan.nextInt();
       if (option == 1){
-        System.out.println("");
+        System.out.println("What would you like to make your item name?");
+        s.setItems();
+        System.out.println("What would you like to make your item description?");
+        System.out.println("What would you like to make your item buyer price?");
       } else if (option == 2){
         System.out.println("");
       } else if (option == 3){
