ab34a70dd2f0de3f775284c991658ac40685baa0
Nikolay Pomytkin <nikolayp2800@gmail.com>
2016-01-12 08:46:10 -0500

    Update storeproject.java

--- ex/groupStore/storeproject.java
+++ ex/groupStore/storeproject.java
@@ -1,1 +1,10 @@
-
+import java.util.scanner;
+public class storeproject{
+  private static void main(String [] args){
+    Scanner scan = new Scanner(System.in);
+    System.out.println("What is the name of your store?");
+    scan.next();
+    System.out.println("Where is your store located?");
+    scan.next();
+  }
+}
