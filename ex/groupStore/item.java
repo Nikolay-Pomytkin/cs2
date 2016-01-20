3ea6f0c9347ed927c710064a0dc9f6cf374a17ba
Nikolay Pomytkin <nikolayp2800@gmail.com>
2016-01-20 09:49:58 -0500

    Update item.java

--- ex/groupStore/item.java
+++ ex/groupStore/item.java
@@ -5,19 +5,22 @@ public class Item {
 	private String description;
 	private double buyer_price;
         private int stock;
+        private int iNum;
   public Item(){
       name = "Item1";
       description = "Great item";
       buyer_price = 1.25;
       stock = 100;
   }
-  public Item(String d, double b, String n, int s) {
-    name = n;
-    description = d;
-    buyer_price = b;
-    stock = s;
+  public Item(String d, double b, String n, int s, int numb) {
+    setName(d);
+    setDescription(d);
+    setBuyerPrice(b);
+    setStock(s);
+    setiNum(numb);
   }
   public void remove(){
+      
       name = null;
       description = null;
       buyer_price = 0;
@@ -35,16 +38,41 @@ public class Item {
   public double getBuyerPrice() {
       return buyer_price;
   }
+  public int getiNum()
+  {
+      return iNum;
+  }
   public void setName(String n){
       name = n;
   }
   public void setDescription(String d){
       description = d;
   }
-  public void setStock(int s){
+  public boolean setStock(int s)
+  {
+      if(s<0)
+      {
+          return false;
+      }
       stock = s;
+      return true;
   }
-  public void setBuyerPrice(double b){
+  public boolean setBuyerPrice(double b)
+  {
+      if(b<0)
+      {
+          return false;
+      }
       buyer_price = b;
+      return true;
+  }
+  public boolean setiNum (int n)
+  {
+      if(n<0)
+      {
+          return false;
+      }
+      iNum = n;
+      return true;
   }
 }
