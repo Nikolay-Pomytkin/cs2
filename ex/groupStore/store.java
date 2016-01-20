8d2ba7cae2d3893a52f5f4d3d709dd593ddf7b87
Nikolay Pomytkin <nikolayp2800@gmail.com>
2016-01-20 09:49:35 -0500

    Update store.java

--- ex/groupStore/store.java
+++ ex/groupStore/store.java
@@ -8,49 +8,78 @@ public class Store {
 	private String owner;
 	private String phone_number;
 	private String name;
+       
 	  
 	public Store() {
             location = "USA";
 	    owner = "Nik";
 	    phone_number = "173-817-3817";
-	}
-	  
+	}	  
 	public Store (String l, Item[] i, String o, String p, String n) {
 	    location = l;
 	    owner = o;
 	    phone_number = p;
 	    name = n;
 	 }
-	 public Item getItem(int id){
-             return items.get(id);
+	 public Item getItem(int id)
+         {
+             for(int ii=0; ii < items.size();ii++)
+             {
+                 Item item=items.get(ii);
+                 if (item.getiNum()==id)
+                 {
+                     return item;
+                 }
+             }
+             return null;
          }
-	 public ArrayList<Item> getItems() {
+         public Item getItem(String name)
+         {
+             for(int ii=0; ii < items.size();ii++)
+             {
+                 Item item=items.get(ii);
+                 if (item.getName().equals(name))
+                 {
+                     return item;
+                 }
+             }
+             return null;
+         }
+	 public ArrayList<Item> getItems() 
+         {
 	    return items;
 	 }
-	 public String getLocation() {
+	 public String getLocation() 
+         {
+             
 	    return location;
 	 }
-	 public String getOwner() {
+	 public String getOwner() 
+         {
 	    return owner;
 	 }	  
-	 public String getPhoneNumber() {
+	 public String getPhoneNumber() 
+         {
 	    return phone_number;
-	 }
+	 }        
 	 //Set 1 item in a certain place in the array
-	 public void setItem(Item i, int z){
-		 items.add(z, i);
-	 }
+	
 	 //Set an array of multiple items
-	 public void setLocation(String l) {
+	 public void setLocation(String l) 
+         {
 	    location = l;
 	 }
-	 public void setOwner(String o) {
+	 public void setOwner(String o) 
+         {
 	    owner = o;
 	 }
-	 public void setPhoneNumber(String p) {
+	 public void setPhoneNumber(String p) 
+         {
 	    phone_number = p;
 	 }
-	 public void setName(String n) {
-		  name = n;
+	 public void setName(String n) 
+         {
+            name = n;
 	 }
+         
 }
